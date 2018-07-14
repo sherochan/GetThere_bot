dc3a31b6b8082d2a73bf5d9bcb5d579388d612e1
sheryl toh <toh152@gmail.com>
2018-07-14 10:17:33 +0800

    Hides API token

--- beta.py
+++ beta.py
@@ -1,5 +1,11 @@
 import django
 import requests
 
+#path = r"C:/Users/toh15/.ssh/test/GetThere_bot_token.txt"
+# getting the API token
+with open("GetThere_bot_token.txt",'r') as myfile:
+    data = myfile.read().replace('\n', '')
+
+print(data)
 r = requests.get('https://api.telegram.org/bot574534905:AAH_w1xrggnjS3fe5yEs6fDf_HXFVJokidQ/getMe')
 print(r.text)
