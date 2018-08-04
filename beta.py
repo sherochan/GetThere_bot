f2517a57b0859070c774852c0696f5213956487f
sheryl toh <toh152@gmail.com>
2018-08-04 23:36:50 +0800

    Change from url request to using telegram bot library
    Partially completed with the code adaptation of the location function

--- beta.py
+++ beta.py
@@ -3,112 +3,155 @@ import requests
 import pdb
 import json
 import time
+import telegram
+#import telebot
+import http.client
+## adapted from https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/conversationbot.py
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler,ConversationHandler
 
-# getting the API token ( static file )
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+GENDER, PHOTO, LOCATION, BIO = range(4)
+
+# getting the API token for telegram bot ( static file )
 with open("GetThere_bot_token.txt",'r') as myfile:
     TOKEN = myfile.read().replace('\n', '')
+
+# getting the API authentication for one map
+with open("OneMap_Auth.txt",'r') as myfile1:
+    OneMap_details = myfile1.read().splitlines()
 
 # calling the bot
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
-def get_Me():
-    """
-    This is a default getMe function from the telegram
-    obtain a text response of bot details
-    :return: {"ok":true,"result":{"id":xxxxxxxxx,"is_bot":true,"first_name":"GetThere_bot","username":"GetThere_bot"}}
-    """
-    getMe_url = URL + "getMe"
-    print(getMe_url)
-    r = requests.get(getMe_url)
-    return r.text
+def start(bot, update):
+    reply_new_keyboard_1 = KeyboardButton("Send location",request_location = True)
+    reply_new_keyboard_2 = KeyboardButton("Search location ")
+    reply_keyboard = [[reply_new_keyboard_1,reply_new_keyboard_2]]
+    logger.info("Run the start function")
+
+    update.message.reply_text(
+        'Welcome to the Get_There_bot! \n\n '
+        'To start with, I would need your location. Or you could do a location search. \n\n'
+        'Send /cancel to stop talking to me.\n\n',
+        reply_markup= ReplyKeyboardMarkup(reply_keyboard,resize_keyboard = True, one_time_keyboard= True))
+
+    user = update.message.from_user
+    print(user)
+    location = update.message.location
+    print(location)
+    return LOCATION
+
+def gender(bot, update):
+    user = update.message.from_user
+    location = update.message.location
+    print("user location?")
+    print(location)
+    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('I see! Please send me a photo of yourself, '
+                              'so I know what you look like, or send /skip if you don\'t want to.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return PHOTO
 
 
-def get_updates_json():
-    """
-    This is a default getUpdates function from telegram
-    obtain json response of the updates which contains info such as update_id,first_name,
-    last_name, username,chat_id, text for when this code runs.
-    anything send to the bot after this code is run will not be captured.
-    :return: the json response
-    """
-    getUpdates_method = URL + "getUpdates"
-    r_updates = requests.get(getUpdates_method)
-    r_updates_json = json.loads(r_updates.text)
-    return r_updates_json
+def photo(bot, update):
+    user = update.message.from_user
+    photo_file = bot.get_file(update.message.photo[-1].file_id)
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text('Gorgeous! Now, send me your location please, '
+                              'or send /skip if you don\'t want to.')
 
+    return LOCATION
+
+
+def skip_photo(bot, update):
+    user = update.message.from_user
+    logger.info("User %s did not send a photo.", user.first_name)
+    update.message.reply_text('I bet you look great! Now, send me your location please, '
+                              'or send /skip.')
+
+    return LOCATION
+
+
+def location(bot, update):
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
+                user_location.longitude)
+    update.message.reply_text('Maybe I can visit you sometime! '
+                              'At last, tell me something about yourself.')
+
+    return
+
+
+def skip_location(bot, update):
+    user = update.message.from_user
+    logger.info("User %s did not send a location.", user.first_name)
+    update.message.reply_text('You seem a bit paranoid! '
+                              'At last, tell me something about yourself.')
+
+    return BIO
+
+
+# def bio(bot, update):
+#     user = update.message.from_user
+#     logger.info("Bio of %s: %s", user.first_name, update.message.text)
+#     update.message.reply_text('Thank you! I hope we can talk again some day.')
 #
-# def get_text_chatID_name_from_json():
-#     """
-#     This function gets the json updates from get_updates_json() function
-#     obtain the last updates and its id, text, and first name
-#     :return: text, chat_id, first name of that update message
-#     """
-#     response_json = get_updates_json()
-#     chat_id = response_json['result'][-1]['message']['chat']['id']
-#     text = response_json['result'][-1]['message']['text']
-#     name = response_json['result'][-1]['message']['from']['first_name']
-#     return text, chat_id, name
-
-def get_text_from_json(_response_json):
-    """
-    This function takes in the json response
-    extracts the text out of latest message
-    :param _response_json:
-    :return: text
-    """
-    text = _response_json['result'][-1]['message']['text']
-    return text
-
-def get_chatID_from_json(_response_json):
-    """
-    This function takes in the json response
-    extracts the chatid out of the latest message
-    :param _response_json:
-    :return: chatid
-    """
-    chatid = _response_json['result'][-1]['message']['chat']['id']
-    return chatid
-
-def get_name_from_json(_response_json):
-    """
-    This function takes in the json response
-    extracts out the name from the latest message
-    :param _response_json:
-    :return: first name , last name
-    """
-    first_name = _response_json['result'][-1]['message']['from']['first_name']
-    last_name =  _response_json['result'][-1]['message']['from']['last_name']
-    return first_name, last_name
+#     return ConversationHandler.END
 
 
+def cancel(bot, update):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
 
-def send_message(_textMessage,_response_json):
-    """
-    takes in a dynamic message as the parameter
-    :param _textMessage:
-    this functions first get the text, chatid and name from the json response
-    combines them in an updated text which adds in the user's name into the textmessage
-    post it back to the chatbot as a response to the user
-    :return: none
-    """
-    first_name, last_name = get_name_from_json(_response_json)
-    chat_id = get_chatID_from_json(_response_json)
+    return ConversationHandler.END
 
-    _updated_text = "Hello {}, ".format(str(first_name)) + _textMessage
-    url = URL + "sendMessage?text={}&chat_id={}".format(_updated_text, str(chat_id))
-    requests.post(url)
 
-response_json = get_updates_json()
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
 def main():
-    last_textchat = (None, None)
-    while True:
-        text = get_text_from_json(response_json)
-        chat = get_chatID_from_json(response_json)
-        if (text, chat) != last_textchat:
-            send_message("How are you?",response_json)
-            last_textchat = (text, chat)
-        time.sleep(0.5)
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(TOKEN)
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            LOCATION: [MessageHandler(Filters.location, location)],
+
+            PHOTO: [MessageHandler(Filters.photo, photo),
+                    CommandHandler('skip', skip_photo)],
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
+    # log all errors
+    dp.add_error_handler(error)
+    # Start the Bot
+    updater.start_polling()
+
+    updater.idle()
 
 
 if __name__ == '__main__':
     main()
+
